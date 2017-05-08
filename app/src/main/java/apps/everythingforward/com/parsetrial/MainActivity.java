74507e4b0bf814e5c10b19b7db89df6fca2cec93
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-08 16:32:38 +0530

    Finished Login

--- app/src/main/java/apps/everythingforward/com/parsetrial/MainActivity.java
+++ app/src/main/java/apps/everythingforward/com/parsetrial/MainActivity.java
@@ -3,17 +3,33 @@ package apps.everythingforward.com.parsetrial;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.parse.Parse;
+import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int LOGIN_CODE=0;
+    ParseUser user=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParseUser.logOut();
         ParseLoginBuilder builder = new ParseLoginBuilder(MainActivity.this);
-        startActivityForResult(builder.build(), 0);
+        startActivityForResult(builder.build(), LOGIN_CODE);
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
 
     }
@@ -21,6 +37,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode)
+        {
+            case LOGIN_CODE:
+            {
+
+
+
+                if(ParseUser.getCurrentUser()!=null)
+                {
+                    user=ParseUser.getCurrentUser();
+                }
+                else
+                {
+                    Toast.makeText(this, "Failed To Login!!", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        }
 
 
     }
